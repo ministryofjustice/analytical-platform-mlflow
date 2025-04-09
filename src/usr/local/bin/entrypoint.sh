430db3248a55681575f3fdda6faf47edb75933c0
Jacob Woffenden <jacob.woffenden@justice.gov.uk>
2025-04-09 13:05:50 +0100

    🚚 Add migration command if database is Postgres (#112)
    Signed-off-by: Jacob Woffenden <jacob.woffenden@justice.gov.uk>

--- src/usr/local/bin/entrypoint.sh
+++ src/usr/local/bin/entrypoint.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -e
+
 export MLFLOW_AUTH_CONFIG_PATH="${MLFLOW_ROOT}/auth.ini"
 export MLFLOW_AUTH_ADMIN_USERNAME="${MLFLOW_AUTH_ADMIN_USERNAME:-"analyticalplatform"}"
 export MLFLOW_AUTH_ADMIN_PASSWORD="${MLFLOW_AUTH_ADMIN_PASSWORD:-"analyticalplatform"}"
@@ -12,17 +14,25 @@ export MLFLOW_SERVER_WORKERS="${MLFLOW_SERVER_WORKERS:-"1"}"
 export MLFLOW_SERVER_BACKEND_STORE_URI="${MLFLOW_SERVER_BACKEND_STORE_URI:-"sqlite:///mlflow.db"}"
 export MLFLOW_SERVER_DEFAULT_ARTIFACT_ROOT="${MLFLOW_SERVER_DEFAULT_ARTIFACT_ROOT:-"${MLFLOW_ROOT}/mlruns"}"
 
+echo "Configuring ${MLFLOW_AUTH_CONFIG_PATH}"
 sed --in-place "s|ADMIN_USERNAME|${MLFLOW_AUTH_ADMIN_USERNAME}|" "${MLFLOW_AUTH_CONFIG_PATH}"
 sed --in-place "s|ADMIN_PASSWORD|${MLFLOW_AUTH_ADMIN_PASSWORD}|" "${MLFLOW_AUTH_CONFIG_PATH}"
 sed --in-place "s|DATABASE_URI|${MLFLOW_AUTH_DATABASE_URI}|" "${MLFLOW_AUTH_CONFIG_PATH}"
 sed --in-place "s|DEFAULT_PERMISSION|${MLFLOW_AUTH_DEFAULT_PERMISSION}|" "${MLFLOW_AUTH_CONFIG_PATH}"
 
 if [[ "${MLFLOW_SERVER_DEV_MODE}" == "true" ]]; then
+  echo "Running in development mode"
   MLFLOW_SERVER_DEV_MODE_FLAG="--dev"
 else
   MLFLOW_SERVER_DEV_MODE_FLAG=""
 fi
 
+if [[ "${MLFLOW_SERVER_BACKEND_STORE_URI}" == "postgresql://"* ]]; then
+  echo "Running database migrations"
+  mlflow db upgrade "${MLFLOW_SERVER_BACKEND_STORE_URI}"
+fi
+
+echo "Starting MLflow server"
 mlflow server ${MLFLOW_SERVER_DEV_MODE_FLAG} \
   --host "${MLFLOW_SERVER_HOST}" \
   --port "${MLFLOW_SERVER_PORT}" \
